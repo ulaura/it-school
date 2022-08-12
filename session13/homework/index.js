216c96597a77e27658f7b6c1160df5490362ed05
Laura Urian <lauraurdaswork@gmail.com>
2022-08-12 12:49:15 +0300

    Homework

---
+++ session13/homework/index.js
@@ -0,0 +1,11 @@
+let fullName = prompt("Please enter your full name");
+let age = prompt("Please enter your age");
+let occupation = prompt("Please enter your occupation");
+let hobby = prompt("Please enter your hobby");
+
+console.log(`
+Name: ${fullName}
+Age: ${age}
+Occupation: ${occupation}
+Hobby: ${hobby} 
+`);
