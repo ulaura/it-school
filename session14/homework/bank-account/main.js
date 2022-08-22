22e9fcc5ebafc68f2eabeceba348162c0b69c99a
Laura Urian <lauraurdaswork@gmail.com>
2022-08-22 16:33:29 +0300

    Session 14 Homework

---
+++ session14/homework/bank-account/main.js
@@ -0,0 +1,48 @@
+let balance = 100;
+
+(() => {
+  console.log(
+    "Available operations:" +
+      "\nW - Withdraw" +
+      "\nC - Check balance" +
+      "\nD - Add money" +
+      "\nNOTE: use execOp(operation key)"
+  );
+})();
+
+const execOp = (operation) => {
+    const op = operation.toUpperCase();
+
+    // (op === "W" && withdraw() ||
+    // op === "C" && checkBalance() ||
+    // op === "A" && deposit()) || alert("Unknown operation");
+
+  switch (operation.toUpperCase()) {
+    case "W":
+      withdraw();
+      break;
+    case "C":
+        checkBalance();
+    break;
+    case "D":
+        deposit();
+        break;
+    default:
+      alert("Unknown operation");
+  }
+};
+
+const withdraw = () => {
+  const amount = Number(prompt("Enter amount to withdraw"));
+  const result = balance - amount;
+  result < 0 ? alert("You have insufficient money") : balance = result;
+};
+
+const checkBalance = () => {
+    alert(`Your balance is: ${balance}`);
+}
+
+const deposit = () => {
+    const amount = Number(prompt("Enter amount to deposit"));
+    balance += amount;
+}
---
+++ session14/homework/while-loop/main.js
@@ -0,0 +1,8 @@
+let dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
+
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log("Loop is about to end...");
+}
